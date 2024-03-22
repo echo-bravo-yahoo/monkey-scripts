5902a938943e64a4a8f6350c800da243f6e9e69c
Ashton Eby <ashton.eby@oracle.com>
2024-03-22 11:53:11 -0700

    (bug) fix import bug

--- zoom-auto-close.user.js
+++ zoom-auto-close.user.js
@@ -2,12 +2,12 @@
 // @name         Zoom auto-close
 // @namespace    http://tampermonkey.net/
 // @for          Zoom
-// @version      0.1.1
+// @version      0.1.2
 // @description  Automatically closes zoom tabs after successfully redirecting you to the zoom app.
 // @author       Ashton Eby (https://github.com/echo-bravo-yahoo)
 // @match        https://*.zoom.us/j/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zoom.us
-// @require      https://raw.githubusercontent.com/echo-bravo-yahoo/monkey-scripts/main/zoom-auto-close.user.js
+// @require      https://raw.githubusercontent.com/echo-bravo-yahoo/monkey-scripts/main/on-dom-change.js
 // @grant        window.close
 // ==/UserScript==
 
@@ -29,6 +29,10 @@
   // sometimes, the greasemonkey script will be run after the hashchange event has already fired
   // so, let's check immediately, as well as on event trigger
   if (window.location.hash === '#success') { window.close() }
+
+  // if the user gave permission for Chrome to launch Zoom via custom application URI, this may
+  // succeed without the user clicking the button, so let's poll on it
+  setInterval(() => { if (window.location.hash === '#success') window.close() }, 1000)
 
   onDomChange(augmentJoinCallButton)
   augmentJoinCallButton()
